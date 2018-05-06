4e6623427d9246fbebc4adf62c2e691924380a01
Grace Shih <vocalise@gmail.com>
2018-05-06 08:32:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,15 @@
 # Write your code here.
-def line
+def line(katz_deli)
+  print "The line is currently: "
+  katz_deli.each_with_index do |name, index|
+    print "#{index+1}. #{name} "
+  end
 end
 
 def take_a_number(katz_deli, name)
+  katz_deli.append("name")
+  puts "Welcome #{name}. You are number #{katz_deli[name].index+1}."
 end
 
 katz_deli = []
-def_line(katz_deli)
+take_a_number(katz_deli)
