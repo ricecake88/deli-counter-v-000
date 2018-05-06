e549d8445f6a80415e61bfba95909c9a1288c08c
Grace Shih <vocalise@gmail.com>
2018-05-06 08:37:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,9 @@
 # Write your code here.
 def line(katz_deli)
+  if katz_deli.count == 0
+    puts "The line is empty"
+    return
+  end
   print "The line is currently: "
   katz_deli.each_with_index do |name, index|
     print "#{index+1}. #{name} "
