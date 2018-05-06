6f8c95c643eaff55e97fe2b10453b4a4fd330dec
Grace Shih <vocalise@gmail.com>
2018-05-06 08:52:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.push("name")
+  katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.count}."
 end
 
